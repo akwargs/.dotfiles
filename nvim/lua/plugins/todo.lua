d2490043cf8363fd5a1ed99535c09e2f9189ce58
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-02 20:07:35 -0500

    add plugins

---
+++ nvim/lua/plugins/todo.lua
@@ -0,0 +1,16 @@
+-- TODO: test
+-- NOTE: this is a test
+-- FIXME: blah
+-- BUG: blah
+-- WARN: blah
+-- HACK: blah
+-- PERF: blah
+-- TEST: test
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "BufRead",
+  config = function()
+    require("todo-comments").setup()
+  end,
+}
