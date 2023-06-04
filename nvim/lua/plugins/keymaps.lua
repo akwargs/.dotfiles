57b91a2968efec508c8712ec3990ac5fbc11b5ba
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-04 08:53:46 -0500

    rnu cuc toggles

---
+++ nvim/lua/plugins/keymaps.lua
@@ -0,0 +1,4 @@
+vim.api.nvim_set_keymap("n", "<leader>RR", ":set rnu!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>RC", ":set cuc!<CR>", { noremap = true, silent = true })
+
+return {}
