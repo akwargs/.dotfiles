685ca2261db435357444f23a2b6f6d91d1ab7dfd
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-11 08:27:45 -0500

    remove RR

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -1,4 +1,3 @@
-vim.api.nvim_set_keymap("n", "<leader>RR", ":set rnu!<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>RC", ":set cuc!<CR>", { noremap = true, silent = true })
 
 return {}
