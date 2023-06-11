f0823785bc6de945fb415140e52082b2519b58d9
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-11 08:28:07 -0500

    show both numbers and relative numbers

--- nvim/lua/plugins/options.lua
+++ nvim/lua/plugins/options.lua
@@ -1,7 +1,9 @@
 vim.opt.backup = true
+vim.opt.cursorcolumn = true
 vim.opt.fileformats = "unix,dos"
 vim.opt.undodir = "."
 vim.opt.mouse = ""
+vim.opt.statuscolumn = "%s %C %l %=%{v:relnum?v:relnum:v:lnum}   "
 
 if vim.g.neovide then
   vim.opt.guifont = "JetBrainsMonoNL Nerd Font Mono:h10"
