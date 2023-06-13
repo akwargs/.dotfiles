402ce512d63e42b8af10f97096c74e1a2c3167f5
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-13 12:47:18 -0500

    indentblankline disabled

--- nvim/lua/plugins/autocmds.lua
+++ nvim/lua/plugins/autocmds.lua
@@ -8,16 +8,16 @@ vim.cmd([[
     au ColorScheme * highlight Pmenu guibg=#ececec
   augroup END
 
-  augroup _CLIAnalyzer
-    au!
-    au BufRead session-capture.*.txt setlocal ft=log
-    au BufRead *.log setlocal ft=log
-  augroup END
+  " augroup _CLIAnalyzer
+  "   au!
+  "   au BufRead session-capture.*.txt setlocal ft=log
+  "   au BufRead *.log setlocal ft=log
+  " augroup END
 
-  augroup _logs
-    au!
-    au filetype log let g:indent_blankline_enabled = v:false
-  augroup end
+  " augroup _logs
+  "   au!
+  "   au filetype log let g:indent_blankline_enabled = v:false
+  " augroup end
 ]])
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
