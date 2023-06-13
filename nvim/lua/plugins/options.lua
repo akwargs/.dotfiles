df2da12e8c5a29501920465710d07ac837a57b11
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-13 08:09:09 -0500

    nottimeout

--- nvim/lua/plugins/options.lua
+++ nvim/lua/plugins/options.lua
@@ -2,9 +2,10 @@ vim.opt.backup = true
 vim.opt.cursorcolumn = false
 vim.opt.cursorline = false
 vim.opt.fileformats = "unix,dos"
-vim.opt.undodir = "."
 vim.opt.mouse = ""
 vim.opt.statuscolumn = "%s %C %l %=%{v:relnum?v:relnum:v:lnum}   "
+vim.opt.ttimeout = false
+vim.opt.undodir = "."
 
 if vim.g.neovide then
   vim.opt.guifont = "JetBrainsMonoNL Nerd Font Mono:h10"
