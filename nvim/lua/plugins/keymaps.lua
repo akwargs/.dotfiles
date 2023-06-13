35da2555bcdbbac3ecee836c4042d8ac5abf6ab3
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-13 08:29:01 -0500

    add NN toggle

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -1,3 +1,4 @@
 vim.api.nvim_set_keymap("n", "<leader>RC", ":set cuc!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>NN", ":IndentBlanklineToggle<CR>", { noremap = true, silent = true })
 
 return {}
