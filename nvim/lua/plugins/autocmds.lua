c4ae78dde8684d9fb7d2ee84892b001285574669
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-13 09:12:57 -0500

    disable indentblankline with logs; cli analyzer is a log

--- nvim/lua/plugins/autocmds.lua
+++ nvim/lua/plugins/autocmds.lua
@@ -1,3 +1,25 @@
+vim.cmd([[
+  augroup _Highlights
+    autocmd!
+    " au ColorScheme * highlight CursorColumn guibg=#c4c8da
+    au ColorScheme * highlight CursorLine guibg=#ececec
+    au ColorScheme * highlight CursorColumn guibg=#ececec
+    au ColorScheme * highlight SignColumn guibg=#ececec
+    au ColorScheme * highlight Pmenu guibg=#ececec
+  augroup END
+
+  augroup _CLIAnalyzer
+    au!
+    au BufRead session-capture.*.txt setlocal ft=log
+    au BufRead *.log setlocal ft=log
+  augroup END
+
+  augroup _logs
+    au!
+    au filetype log let g:indent_blankline_enabled = v:false
+  augroup end
+]])
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "json" },
   callback = function()
@@ -5,15 +27,11 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
-vim.cmd([[
-  augroup _Highlights
-  autocmd!
-  " au ColorScheme * highlight CursorColumn guibg=#c4c8da
-  au ColorScheme * highlight CursorLine guibg=#ececec
-  au ColorScheme * highlight CursorColumn guibg=#ececec
-  au ColorScheme * highlight SignColumn guibg=#ececec
-  au ColorScheme * highlight Pmenu guibg=#ececec
-  augroup END
-]])
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "json" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
 
 return {}
