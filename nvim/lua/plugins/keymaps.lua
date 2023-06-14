d6c06f10fbbac4446ba34fd044531757afabcc53
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-14 13:43:09 -0500

    add :bp and :bn keymaps

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_set_keymap("n", "<leader>RC", ":set cuc! cul!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-PageUp>", ":bp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-PageDown>", ":bn<CR>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "<leader>NN", ":IndentBlanklineToggle<CR>", { noremap = true, silent = true })
 
 return {}
