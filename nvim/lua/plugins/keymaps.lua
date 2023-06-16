cda6d390bef9fdf4b9d006ca01844e1acce635cc
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-06-16 08:40:05 -0500

    bp bn while in insert mode

--- nvim/lua/plugins/keymaps.lua
+++ nvim/lua/plugins/keymaps.lua
@@ -1,6 +1,8 @@
 vim.api.nvim_set_keymap("n", "<leader>RC", ":set cuc! cul!<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-PageUp>", ":bp<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-PageDown>", ":bn<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-PageUp>", "<ESC>:bp<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-PageDown>", "<ESC>:bn<CR>", { noremap = true, silent = true })
 -- vim.api.nvim_set_keymap("n", "<leader>NN", ":IndentBlanklineToggle<CR>", { noremap = true, silent = true })
 
 return {}
