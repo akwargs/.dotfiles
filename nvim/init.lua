e6836bfa2cd58e2d73fc1d1d11dda84d650bdca7
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-07-09 16:08:43 -0500

    import updated init

---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+vim.cmd([[
+  let g:python3_host_prog = '/usr/local/Python/pyenv/versions/py3nvim/bin/python'
+  " let g:python3_host_prog = '/usr/bin/python3'
+]])
+require("config.lazy")
