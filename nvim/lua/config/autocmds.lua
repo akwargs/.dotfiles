dec525e5cd950595eca5c0b803b36984a0b07c09
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2023-07-10 09:42:39 -0500

    add highlight on yank and help ft

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -9,6 +9,7 @@ vim.cmd([[
     au ColorScheme * highlight CursorColumn guibg=#ececec
     " au ColorScheme * highlight CursorColumn guibg=#c4c8da
     au ColorScheme * highlight SignColumn guibg=#ececec
+    " au ColorScheme * highlight ColorColumn guibg=#ececec
     au ColorScheme * highlight Pmenu guibg=#ececec
     au ColorScheme * highlight WhiteSpace guifg=#c4c8da
     au ColorScheme * set background=light
@@ -38,6 +39,23 @@ vim.cmd([[
     au filetype help setlocal cul! cuc!
   augroup end
 ]])
+
+local augroup = vim.api.nvim_create_augroup("user_cmds", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "help", "man" },
+	group = augroup,
+	desc = "Use q to close the window",
+	command = "nnoremap <buffer> q <cmd>quit<cr>",
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = augroup,
+	desc = "Highlight on yank",
+	callback = function(event)
+		vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+	end,
+})
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "json" },
