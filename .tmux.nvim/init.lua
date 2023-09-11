e22717499bd6c1daa3df3a9014bc068d3fa926ca
Anthony <135379770+akwargs@users.noreply.github.com>
2023-09-10 22:20:02 -0500

    add reference to github

--- .tmux.nvim/init.lua
+++ .tmux.nvim/init.lua
@@ -1,3 +1,4 @@
+-- git clone https://github.com/spywhere/tmux.nvim .tmux.nvim
 vim.opt.rtp:append({'~/.tmux.nvim', '~/vimfiles', '~/.vim'})
 local tmux = require('tmux')
 vim.cmd [[source ~/vimfiles/vimrc]]
