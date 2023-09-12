7de7bf477276f5ae393e34eca47e642dd4401bdf
Anthony <135379770+akwargs@users.noreply.github.com>
2023-09-12 07:23:04 -0500

    remove unneeded path from rtp

--- .tmux.nvim/init.lua
+++ .tmux.nvim/init.lua
@@ -1,5 +1,5 @@
 -- git clone https://github.com/spywhere/tmux.nvim .tmux.nvim
-vim.opt.rtp:append({'~/.tmux.nvim', '~/vimfiles', '~/.vim'})
+vim.opt.rtp:append({'~/.tmux.nvim', '~/vimfiles'})
 local tmux = require('tmux')
 vim.cmd [[source ~/vimfiles/vimrc]]
 tmux.start()
