77103c60b8761ca1f802870fa40f6466412aeea9
Anthony Kim <135379770+akwargs@users.noreply.github.com>
2025-05-06 12:24:37 -0500

    migrate to just lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd([[
-let g:loaded_perl_provider = 0
-if !empty($NVIM_PYTHON3_HOST)
-  let g:python3_host_prog = "$NVIM_PYTHON3_HOST"
-endif
-if !empty($NVIM_NODE_HOME)
-  let g:node_host_prog = "$NVIM_NODE_HOST"
-endif
-]])
-
-local opt = vim.opt
-opt.mouse = "n"
+vim.g.loaded_perl_provider = 0
+vim.g.node_host_prog = vim.env.NVIM_NODE_HOST
+vim.g.python3_host_prog = vim.env.NVIM_PYTHON3_HOST
+vim.opt.mouse = "n"
