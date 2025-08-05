79787ad3a52eaf16b3b320d86cfe83ec06ab500a
Anthony <135379770+akwargs@users.noreply.github.com>
2025-08-04 19:43:48 -0500

    add noremap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -12,8 +12,8 @@ vim.keymap.set("n", "<C-l>", ":nohl<CR><C-l>", opts)
 
 -- show columns at 80 and 120
 vim.keymap.set("n", "<leader>ro", [[:exe "set cc=" . (&cc == "" ? "80,120" : "")<CR>]], opts)
-vim.keymap.set("n", "<leader>rl", [[:exe "set cuc! cul!"<CR>]])
-vim.keymap.set("n", "<leader>rr", [[:exe "set rnu! nu!"<CR>]])
+vim.keymap.set("n", "<leader>rl", [[:exe "set cuc! cul!"<CR>]], opts)
+vim.keymap.set("n", "<leader>rr", [[:exe "set rnu! nu!"<CR>]], opts)
 
 -- cursor remains in place after J
 vim.keymap.set("n", "J", [[mzJ`z]], opts)
